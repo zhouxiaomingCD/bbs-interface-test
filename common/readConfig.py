4b7ee566122867342b89d4c04e21ddc03e0ef8b2
周小明 <1258416887@qq.com>
2020-11-12 16:37:22 +0800

    注释readConfig.py-line13 因codecs引起的编码问题

--- common/readConfig.py
+++ common/readConfig.py
@@ -9,19 +9,18 @@ configPath = os.path.join(proDir, "conf/config.ini")
 
 class ReadConfig:
     def __init__(self):
-        # fd = open(configPath, "r")
-        # data = fd.read()
-        # print(data)
-        # #  remove BOM
-        # if data[:3] == codecs.BOM_UTF8:
-        #     data = data[3:]
-        #     file = codecs.open(configPath, "w")
-        #     file.write(data)
-        #     file.close()
-        # fd.close()
+        fd = open(configPath, encoding="utf-8")
+        data = fd.read()
+        #  remove BOM
+        if data[:3] == codecs.BOM_UTF8:
+            data = data[3:]
+            file = codecs.open(configPath, "w")
+            file.write(data)
+            file.close()
+        fd.close()
 
         self.cf = configparser.ConfigParser()
-        self.cf.read(configPath)
+        self.cf.read(configPath, encoding="utf-8")
 
     def get_email(self, name):
         value = self.cf.get("EMAIL", name)
